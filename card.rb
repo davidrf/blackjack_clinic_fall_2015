eea37c219531bdd99b6194bb820be74aed5a2c93
David Rodriguez Fuentes <davidrf09@gmail.com>
2015-09-01 15:56:47 -0400

    Beginning of clinic

--- card.rb
+++ card.rb
@@ -9,24 +9,4 @@ class Card
   def name
     "#{@value}#{@suit}"
   end
-
-  def ace?
-    "A" == value
-  end
-
-  def face_card?
-    ["J", "Q", "K", "A"].include?(value)
-  end
-
-  def numeric_value
-    if face_card?
-      if ace?
-        1
-      else
-        10
-      end
-    else
-      value.to_i
-    end
-  end
 end
